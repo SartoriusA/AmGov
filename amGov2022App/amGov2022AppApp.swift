31dd479b389b063d56a79f9ec0fac7f96e2b8367
Andrew Sartorius <sartoriusa@live.com>
2022-06-23 16:06:28 -0500

    Initial Commit

---
+++ amGov2022App/amGov2022AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  amGov2022AppApp.swift
+//  amGov2022App
+//
+//  Created by Andrew J. Sartorius on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct amGov2022AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
